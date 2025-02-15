bf802e93c48c280dcf63d7a11e647d257677b3cd
bekatru <teamusicband@gmail.com>
2025-02-15 11:20:38 +0600

    Show currency on accounts

--- src/screens/AccountsScreen.tsx
+++ src/screens/AccountsScreen.tsx
@@ -9,21 +9,25 @@ export const AccountsScreen = () => {
     <div id="transactions-screen-container" className="h-full flex flex-col justify-end">
         <div className="max-h-full overflow-scroll">
 
-            {Object.values(accounts).map(({ id, name, balance }) => (
+            {Object.values(accounts).map(({ id, name, balance, currency }) => (
                 
                 <div key={id} className="flex flex-row justify-between py-4 mx-4 text-xl font-extralight">
                     <div className={"text-gray-700"}>{name}</div>
-                    <div className={balance >= 0 ? "text-green-500" : "text-red-500"}>{balance}</div>
+                    <div className={balance > 0 ? "text-green-500" : balance < 0 ? "text-red-500" : "text-black"}>
+                        {balance}{" "}{currency}
+                    </div>
                 </div>
             
             ))}
                         
         </div>
+
         <NavLink to={"/create/account"}>
             <div className="py-4 mx-4 border-t border-gray-500 flex justify-center">
                 <Plus size={28} strokeWidth={1} />
             </div>   
         </NavLink>
+        
     </div>
   );
 };
