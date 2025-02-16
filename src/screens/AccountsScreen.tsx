1319a2776be7a52e6208f5b658c4b1e7c5b4f261
bekatru <teamusicband@gmail.com>
2025-02-16 19:37:08 +0600

    Add totals to the AccountsScreen

--- src/screens/AccountsScreen.tsx
+++ src/screens/AccountsScreen.tsx
@@ -1,19 +1,28 @@
+import { useMemo } from "react";
 import { useStore } from "../services/zustand";
 
 export const AccountsScreen = () => {
   const { accounts } = useStore();
 
+  const totals: Record<string, number> = useMemo(() => {
+    return Object.values(accounts).reduce((result, account) => {
+      if (result[account.currency]) {
+        result[account.currency] += account.balance;
+      } else {
+        result[account.currency] = account.balance;
+      }
+      return result;
+    }, {} as Record<string, number>);
+  }, [accounts]);
+
   return (
     <div
       id="transactions-screen-container"
-      className="h-full flex flex-col justify-end"
+      className="h-full flex flex-col justify-end text-xl font-light"
     >
       <div className="max-h-full overflow-scroll">
         {Object.values(accounts).map(({ id, name, balance, currency }) => (
-          <div
-            key={id}
-            className="flex flex-row justify-between py-3 mx-4 text-xl font-light"
-          >
+          <div key={id} className="flex justify-between py-3 mx-4 ">
             <div className={"text-gray-700"}>{name}</div>
             <div
               className={
@@ -30,7 +39,24 @@ export const AccountsScreen = () => {
         ))}
       </div>
 
-      <div className="mb-4 mx-4 border-t flex justify-center"></div>
+      <div className="mb-4 mx-4 border-t flex justify-between py-3">
+        <div className={"text-gray-700"}>totals</div>
+        <div className="flex space-x-4">
+          {Object.entries(totals).map(([currency, balance]) => (
+            <div
+              className={
+                balance > 0
+                  ? "text-green-500"
+                  : balance < 0
+                  ? "text-red-500"
+                  : "text-black"
+              }
+            >
+              {balance} {currency}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
