d2c8dc4080119a491e62152535f2a26f6d9e599f
bekatru <teamusicband@gmail.com>
2025-02-24 14:17:30 +0600

    move firebase config to env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,10 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import firebaseConfig from "../../firebase-config.json";
 import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase app
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_RTBD_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+});
 
 // Export Firebase services
 export const auth = getAuth(app);
